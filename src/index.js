7b31fb7f3307ca03e1e5668f9420a3fe9a5a18d2
fyr-fyrrr <llenasakalova@gmail.com>
2018-09-30 17:22:02 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  const a = (Math.floor(number / 5)) * 5;
+	let b = Math.log10(a) / Math.log10(5);
+	b = Math.floor(b);
+	const arr = [];
+	for (i = 1; i <= b; i++) {
+		arr[i - 1] = Math.floor(number / Math.pow(5,i));
+	};
+	let zerosCount = 0;
+	for(var i = 0; i < arr.length; i++){
+		zerosCount += arr[i];
+    }
+	return(zerosCount);
 }
